8fe3469e41ced2156f3483ab8d3853aa08afe245
dzen <den.leonchev@gmail.com>
2018-04-01 21:35:48 +0300

    Added stg, fixed autologin on reload

--- vue-reviews/src/data/firebase-config.js
+++ vue-reviews/src/data/firebase-config.js
@@ -1,14 +1,23 @@
 import firebase from 'firebase';
 
-var firebaseConfig = {
+const firebaseConfigProd = {
     apiKey: "AIzaSyDgQjpMQf2p421ymTNbY-ZBf_giRphPUFw",
     authDomain: "codereview-vue.firebaseapp.com",
     databaseURL: "codereview-vue.firebaseio.com",
     messagingSenderId: "463670284887",
     senderId : "463670284887"
-};
+}
 
-var theApp = firebase.initializeApp(firebaseConfig)
+const firebaseConfigDev = {
+    apiKey: "AIzaSyAjuwS1LjiJnauxfJe5W3xiVDXstR1YrF0",
+    authDomain: "dev-codereview-vue.firebaseapp.com",
+    databaseURL: "dev-codereview-vue.firebaseio.com",
+    messagingSenderId: "432005203093",
+    senderId:  "432005203093"
+}
+
+const isDev = location.hostname.match(/dev|localhost/i)
+const theApp = firebase.initializeApp(isDev ? firebaseConfigDev : firebaseConfigProd)
 
 export const messaging = theApp.messaging()
 export const FBApp = theApp.database()
